3219a085444ed681e281c05c8d1e57f9fb7fb1da
KharismaIntanSafitri <kharismaintan2001@gmail.com>
2022-03-30 19:07:41 -0800

    menambah folder koneksi database

---
+++ connect/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+// menghubungkan ke database dengan format
+// mysqpli_connect('hostname','username database','password','nama database')
+$koneksi = mysqli_connect('localhost','root','','kharisma');
+// if($koneksi){
+//     echo "Koneksi Berhasiil!!!";
+// }else{
+//     echo "Koneksi Gagal!!!";
+// }
+?>
