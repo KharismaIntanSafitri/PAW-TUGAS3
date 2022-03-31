0e9d89aa1f83d0db7151d58a955a04a8043083e3
KharismaIntanSafitri <kharismaintan2001@gmail.com>
2022-03-30 19:04:02 -0800

    file form update tamu

---
+++ edit_tamu.php
@@ -0,0 +1,73 @@
+<?php
+include "connect/koneksi.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin | Dashboard</title>
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+</head>
+<body>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <?php include "desain/sidebar_admin.php";?>
+    <?php include "desain/navbar_admin.php";?>
+    <?php
+    $id = $_GET['id'];
+    $data = mysqli_fetch_assoc(mysqli_query($koneksi,"SELECT * from tbl_010 where id='$id'"));
+    ?>
+
+    <!-- Awal Isi Konten -->
+    <div class="container-fluid">
+        <!-- Data Tabel -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Tambah Barang</h6>
+            </div>
+            <div class="card-body">
+                <form action="update_tamu.php?id=<?= $id?>" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $data['id']?>">
+                    <div class="form-group row">
+                      <label  class="col-sm-2 col-form-label">Nama</label>
+                      <div class="col-md-3">
+                        <input type="text" class="form-control" name="nama" value="<?= $data['nama']?>">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label class="col-sm-2 col-form-label">No. Hp</label>
+                      <div class="col-md-3">
+                        <input type="text" class="form-control" name="hp" value="<?= $data['hp']?>">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label  class="col-sm-2 col-form-label">Alamat</label>
+                      <div class="col-md-3">
+                        <input type="text" class="form-control" name="alamat" value="<?= $data['alamat']?>">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary" name="update_tamu">Update</button>
+                        <a href="index.php"><button type="button" class="btn btn-primary" name="batal">Batal</button></a>
+                      </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../js/sb-admin-2.min.js"></script>
+    <!-- Page level plugins -->
+    <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/datatables-demo.js"></script>
+</body>
+</html>
